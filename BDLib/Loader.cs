19af8db2c317d48e26f619dcd6b5a36b062e2402
Chair6798 <oggh.original@yandex.ru>
2026-04-28 16:09:40 +0300

    Добавьте файлы проекта.

---
+++ BDLib/Loader.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BDLib
+{
+    [BepInPlugin("com.coolchair.BDLibLoader", "BDLibLoader", "1.0.0")]
+    public class BDLibLoaderPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("BDLib pathing starting...");
+            var harmony = new Harmony("com.coolchair.BDLibLoader");
+
+            harmony.PatchAll();
+            Logger.LogInfo("BDLib was patched successful!");
+        }
+    }
+}
---
+++ BDLib/MainModule.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+
+namespace BDLib
+{
+    public class MainModule
+    {
+
+    }
+}
